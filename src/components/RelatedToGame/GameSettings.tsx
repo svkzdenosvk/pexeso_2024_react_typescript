b9cffc96dfc71003662185a935d319915430addc
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2025-04-23 16:24:03 +0200

    rewritten RelatedToGame css to styled comp

--- src/components/RelatedToGame/GameSettings.tsx
+++ src/components/RelatedToGame/GameSettings.tsx
@@ -1,10 +1,14 @@
-import React, { useState, useRef } from "react";
-import { useEffect } from "react";
-
-import "@pexeso/components/RelatedToGame/css/gameSettings.css";
-
+// GameSettings.tsx
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import styled, { keyframes } from "styled-components";
 
+import {
+  settings_and_styling_before_start,
+  reset_settings,
+} from "@pexeso/store/reducers/gameSlice";
+import { seconds_reset } from "@pexeso/store/reducers/secondsSlice";
 import {
   My_Type_ImgCount,
   My_Type_Level,
@@ -15,154 +19,137 @@ import {
   my_Type_Guard_function_number,
 } from "@pexeso/_inc/_inc_functions";
 
-import { useDispatch } from "react-redux";
+const pulseShadow = keyframes`
+  0% { box-shadow: 0 2px 0px white; }
+  50% { box-shadow: 0 6px 10px goldenrod; }
+  100% { box-shadow: 0 2px 0px white; }
+`;
 
-import {
-  settings_and_styling_before_start,
-  reset_settings,
-} from "@pexeso/store/reducers/gameSlice";
-import { seconds_reset } from "@pexeso/store/reducers/secondsSlice";
+const Form = styled.form`
+  min-width: 350px;
+`;
+
+const Fieldset = styled.fieldset`
+  border-radius: 25px;
+  & + & { margin-top: 10px; }
+`;
+
+const Legend = styled.legend`
+  font-weight: bold;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  font-weight: bold;
+`;
+
+const SubmitButton = styled.button`
+  text-decoration: none;
+  border: none;
+  background: transparent;
+  color: black;
+  margin-top: 10px;
+  font-weight: bold;
+  padding: 10px 25px;
+  display: inline-block;
+  border-radius: 25px;
+  animation: ${pulseShadow} 1.5s infinite ease-in-out;
+
+  &:hover {
+    color: goldenrod;
+    transition: color 0.3s ease;
+    box-shadow: 0px 7px 10px grey;
+  }
+`;
 
 const GameSettings = () => {
-  //----------------------------redux
-
   const dispatch = useDispatch();
-
-  //----------------------------useState
-
-  const [levelChosen, setlevelChosen] = useState("" as My_Type_Level);
-  // const [selectedImages, setSelectedImages] = useState([]); //--------------------choosen images
-  const [imgCountChosen, setimgCountChosen] = useState(0 as My_Type_ImgCount); //----count of choosen images
-  const [error, setError] = useState("");
-
   const navigate = useNavigate();
+  const location = useLocation();
   const formRef = useRef<HTMLFormElement>(null);
 
-  const location = useLocation(); // ------------------------------------------------watching URL
+  const [levelChosen, setLevelChosen] = useState("" as My_Type_Level);
+  const [imgCountChosen, setImgCountChosen] = useState(0 as My_Type_ImgCount); //----count of choosen images
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    dispatch(seconds_reset()); //----------------------------------------------------reset seconds
-    dispatch(reset_settings()); //---------------------------------------------------reset settings
-  }, [location.pathname, dispatch]); // ---------------------------------------------trigger when path change
+    dispatch(seconds_reset());
+    dispatch(reset_settings());
+  }, [location.pathname, dispatch]);
 
-  const imgCount_values: My_Type_ImgCount[] = [5, 6, 7, 8]; // ----------------------count of images for game
-
+  const imgCountValues: My_Type_ImgCount[] = [5, 6, 7, 8];
   const levels: My_Type_Svk_Eng_level[] = [
     { value: "easy", label: "Ľahký" },
     { value: "medium", label: "Stredný" },
     { value: "hard", label: "Ťažký" },
   ];
-
-  const levels_values: My_Type_Level[] = ["easy", "medium", "hard"];
-
-  // const imageOptions = ["vesmir", "kvapka", "more", "sun", "vibracia", "vietor", "drevo", "blesk"];
-
-  // let levelChosen="";
-  // let imgCountChosen=null;
-  // const handleImageSelection = (e) => {
-  //   const { value, checked } = e.target;
-  //   if (checked) {
-  //     setSelectedImages((prev) => [...prev, value]);
-  //   } else {
-  //     setSelectedImages((prev) => prev.filter((img) => img !== value));
-  //   }
-  // };
+  const levelValues: My_Type_Level[] = ["easy", "medium", "hard"];
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    /*-------------------after submit function */
     e.preventDefault();
 
-    if (!my_Type_Guard_function(levelChosen, levels_values)) {
+    if (!my_Type_Guard_function(levelChosen, levelValues)) {
       setError("Nastav level obtiažnosti");
       return;
-    } else if (
-      !my_Type_Guard_function_number(imgCountChosen, imgCount_values)
-    ) {
+    }
+    if (!my_Type_Guard_function_number(imgCountChosen, imgCountValues)) {
       setError("Nastav počet obrázkov, s ktorými chceš hrať.");
       return;
-    } else {
-      setError(""); //----------------------------------------------------------------reset error message
     }
+    setError("");
 
     dispatch(
       settings_and_styling_before_start({
-        //------------------------------------set mainly level and count of images to play with
-        level: levelChosen as My_Type_Level,
-        selectedImgCount: imgCountChosen as My_Type_ImgCount,
-      }),
+        level: levelChosen,
+        selectedImgCount: imgCountChosen,
+      })
     );
-
     formRef.current?.reset();
-
-    navigate(
-      "/game",
-    ); /*---------------------------------------------------------------navigate to save data in useContext*/
+    navigate("/game");
   };
 
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
+    <Form ref={formRef} onSubmit={handleSubmit}>
       <h2>Nastavte parametre hry</h2>
 
-      <fieldset>
-        <legend>Vyberte úroveň obtiažnosti:</legend>
-        {/* -------------------------------choose level */}
-        {levels.map((level_name, index) => (
-          <label key={index}>
+      <Fieldset>
+        <Legend>Vyberte úroveň obtiažnosti:</Legend>
+        {levels.map((lvl, i) => (
+          <label key={i}>
             <input
               type="radio"
               name="level"
-              value={level_name.value}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setlevelChosen(e.target.value as My_Type_Level)
+              value={lvl.value}
+              onChange={(e) =>
+                setLevelChosen(e.target.value as My_Type_Level)
               }
             />
-            {level_name.label}
+            {lvl.label}
           </label>
         ))}
-      </fieldset>
+      </Fieldset>
 
-      <fieldset>
-        <legend>Vyberte počet obrázkov:</legend>
-        {/* ------------------------------------choose count of images to play*/}
-        {imgCount_values.map((value, index) => (
-          <label key={index}>
+      <Fieldset>
+        <Legend>Vyberte počet obrázkov:</Legend>
+        {imgCountValues.map((cnt, i) => (
+          <label key={i}>
             <input
               type="radio"
               name="imageCount"
-              value={value}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setimgCountChosen(parseInt(e.target.value) as My_Type_ImgCount)
+              value={cnt}
+              onChange={(e) =>
+                setImgCountChosen(parseInt(e.target.value) as My_Type_ImgCount)
               }
             />
-            {value * 2}{" "}
-            {/* ------------------------------------------------------------pair is 5 * 2 = 10) */}
+            {cnt * 2}
           </label>
         ))}
-      </fieldset>
+      </Fieldset>
 
-      {/* Checklist of images
-      <fieldset>
-        <legend>Vyberte obrázky:</legend>
-        {imageOptions.map((image) => (
-          <label key={image}>
-            <input
-              type="checkbox"
-              value={image}
-              onChange={handleImageSelection}
-            />
-            {image}
-          </label>
-        ))}
-      </fieldset> */}
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
-      {
-        error && (
-          <p>{error}</p>
-        ) /*-----------------------------------------------------error message */
-      }
-
-      <button type="submit">Hraj</button>
-    </form>
+      <SubmitButton type="submit">Hraj</SubmitButton>
+    </Form>
   );
 };
 
